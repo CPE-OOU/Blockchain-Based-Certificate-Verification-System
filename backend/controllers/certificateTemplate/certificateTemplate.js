a76b874a7b5153e95eb9941595f4392b0ca34663
Ayọ̀mídé Adéníyì Lawal <ayomidelawal700@gmail.com>
2023-12-07 20:21:07 +0100

    change pdf generator

--- backend/controllers/certificateTemplate/certificateTemplate.js
+++ backend/controllers/certificateTemplate/certificateTemplate.js
@@ -1,74 +1,50 @@
+const PDFDocument = require('pdfkit');
+const fs = require('fs');
+
 module.exports = function certificateTemplate(data) {
-  return `
-    <!DOCTYPE html>
-    <html>
-    <head>
-    <style>
-    body {
-      font-family: Arial, sans-serif;
-      margin: 0;
-      padding: 0;
-      background: linear-gradient(to right, #a6ffcb, #12d8fa, #1fa2ff);
-  }
-  .certificate {
-      width: 80%;
-      margin: 50px auto;
-      padding: 30px;
-      background: #fff;
-      text-align: center;
-      border-radius: 20px;
-      border: 10px double #ddd;
-      box-shadow: 0px 0px 30px rgba(0,0,0,0.15);
-  }
-  .university {
-      color: #3c3c3c;
-      font-size: 40px;
-  }
-  .degree {
-      color: #696969;
-      font-size: 35px;
-  }
-  .name {
-      color: #ff6347;
-      font-size: 45px;
-      margin-top: 40px;
-  }
-  .course {
-      color: #696969;
-      font-size: 35px;
-  }
-  .date {
-      color: #696969;
-      font-size: 25px;
-      margin-top: 40px;
-  }
-  .signature {
-      color: #696969;
-      font-size: 25px;
-      margin-top: 60px;
-  }
-  .seal {
-      /* position:absolute;  */
-      /* bottom:20px;  */
-      /* right:20px;  */
-      width:120px; 
-      height:auto
-  }
-</style>
-    </head>
-    <body style="position: fixed; top: 0; bottom: 0; left: 0; right: 0; background: linear-gradient(to right, #a6ffcb, #12d8fa, #1fa2ff);">
-        <div class="certificate">
-            <h1 class="university">Olabisi Onabanjo University</h1>
-            <h2 class="degree">${data.degreeType} Certificate</h2>
-            <img src="../seal1.png" alt="Seal" class="seal">
-            <h2 class="subtitle">This is to certify that</h2>
-            <h2 class="name">${data.firstname} ${data.lastname} ${data.middlename}</h2>
-            <h2 class="subtitle">has successfully completed the Bachelor's degree in</h2>
-            <h2 class="course">${data.courseName}</h2>
-            <p class="date">Year of Completion : ${data.yearOfCompletion}</p>
-            <p class="signature">Signature : [Signature]</p>
-        </div>
-    </body>
-    </html>
-  `;
+ const doc = new PDFDocument();
+
+// Set document margins
+doc.margins({ top: 50, bottom: 50, left: 50, right: 50 });
+
+// Set background color
+doc.fill("#a6ffcb");
+doc.rect(0, 0, doc.page.width, doc.page.height).fill();
+
+// Add certificate content
+doc.fill("#fff");
+doc.rect(0, 50, doc.page.width, doc.page.height - 100).fill();
+
+// University name
+doc.fontSize(40);
+doc.text("Olabisi Onabanjo University", doc.page.width / 2, 70, { align: "center" });
+
+// Degree type
+doc.fontSize(35);
+doc.text(`${data.degreeType} Certificate`, doc.page.width / 2, 120, { align: "center" });
+
+// Seal image
+doc.image(path.join(__dirname, "../seal1.png"), doc.page.width - 150, 100, { width: 120 });
+
+// Subtitle
+doc.fontSize(25);
+doc.text("This is to certify that", doc.page.width / 2, 170, { align: "center" });
+
+// Name
+doc.fontSize(45);
+doc.text(`${data.firstname} ${data.middlename} ${data.lastname}`, doc.page.width / 2, 220, { align: "center" });
+
+// Course name
+doc.text("has successfully completed the Bachelor's degree in", doc.page.width / 2, 270, { align: "center" });
+doc.text(`${data.courseName}`, doc.page.width / 2, 320, { align: "center" });
+
+// Year of completion
+doc.text(`Year of Completion: ${data.yearOfCompletion}`, doc.page.width / 2, 370, { align: "center" });
+
+// Signature
+doc.text("Signature: [Signature]", doc.page.width / 2, 420, { align: "center" });
+
+// Generate PDF file
+doc.pipe(fs.createWriteStream('certificate.pdf'));
+doc.end();
 };
